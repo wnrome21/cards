5a9fa15f0a66837e66987c101f28a5ac490ff6b6
wnrome21 <nateromero001@gmail.com>
2022-12-01 16:17:39 -0800

    added testing to app. tests for card amounts and tests our file reading

---
+++ deck_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 52  {
+		t.Errorf("Expected deck length of 52, but got %v", len(d))
+	}
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades %v", d[0])
+	}
+	if d[len(d)- 1] != "King of Clubs" {
+		t.Errorf("Expected last card of King of Clubs %v", d[len(d) -1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesing")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected 52 cards in deck, got %v", len(loadedDeck))
+	}
+	os.Remove("_decktesting")
+}
